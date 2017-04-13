45d74e975160f7510f573b695a454eaa2bad0719
Enovision GmbH <j.vd.merwe@enovision.net>
2017-04-13 14:13:21 +0200

    Update autoload.php
    Moved the autoload from the 'vendor' from libraries to helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('vendor');
+$autoload['libraries'] = array();
 
 
 /*
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('vendor');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array();
+$autoload['helper'] = array('vendor');
 
 
 /*
